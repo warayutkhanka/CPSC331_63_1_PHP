f88884e90f99daeb3f19e83f2754354767d1b8d4
Warayut <45837597+warayutkhanka@users.noreply.github.com>
2020-08-20 23:08:04 +0700

    Add files via upload

---
+++ test index.htm/three.php
@@ -0,0 +1,2 @@
+<iframe src="http://www.facebook.com/plugins/likebox.php?href=http%3A%2F%2Fwww.facebook.com%2FDoonungTN%3Fref%3Dsgm%23%21%2FDoonungTN%3Fref%3Dsgm&width=400&connections=9&stream=true&header=true&height=550" 
+scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:300px; height:550px;" allowTransparency="true"></iframe>
