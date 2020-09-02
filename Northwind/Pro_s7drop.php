6aa5ab17ecab524190710b76b353894d837c8483
Warayut <45837597+warayutkhanka@users.noreply.github.com>
2020-09-03 00:55:37 +0700

    Add files via upload

---
+++ Northwind/Pro_s7drop.php
@@ -0,0 +1,84 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<title>Northwind</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+body,h1,h2,h3,h4,h5,h6 {font-family: "Lato", sans-serif}
+.w3-bar,h1,button {font-family: "Montserrat", sans-serif}
+.fa-anchor,.fa-coffee {font-size:200px}
+</style>
+<body>
+
+<!-- Navbar -->
+<div class="w3-container">
+    <button> <a href="index.php" class="w3-bar-item w3-button w3-padding-large w3-white">Home</a> </button>
+    <button> <a href="pro_s2crtdb.php" class="w3-bar-item w3-button w3-padding-large w3-white">Crtdb</a> </button>
+    <button> <a href="pro_s3select.php" class="w3-bar-item w3-button w3-padding-large w3-white">Select</a> </button>
+    <button> <a href="pro_s4insert.php" class="w3-bar-item w3-button w3-padding-large w3-white">Insert</a> </button>
+    <button> <a href="pro_s5delete.php" class="w3-bar-item w3-button w3-padding-large w3-white">Delete</a> </button>
+    <button> <a href="pro_s6update.php" class="w3-bar-item w3-button w3-padding-large w3-white">Update</a> </button>
+    <button> <a href="pro_s7drop.php" class="w3-bar-item w3-button w3-padding-large w3-white">Drop</a> </button>
+</div>
+
+
+  
+  
+
+<!-- Header -->
+<header class="w3-container w3-gray w3-center" style="padding:350px 16px">
+<?php
+/* updated for php7 and php 5 on 2560-09-13 */
+require("pro_s1connect.php");
+$sql = "drop database $db";
+if((int)phpversion() >= 7) {
+	$connect->close();
+	$connect = mysqli_connect($host, $uname, $upass);
+	if (mysqli_query($connect, $sql)) 
+		echo "Drop database : succeeded";
+	else 
+		echo "Drop database : failed " . mysqli_error($connect);
+	$connect->close();
+} else {
+	if (!$result=mysql_query($sql,$connect))
+		echo "Drop database : failed";
+	else
+		echo "Drop database : succeeded";
+	mysql_close($connect);
+}
+?>
+<input type='button' onclick="location.href='index.php'" value='back'>
+</header>
+
+
+
+<!-- Footer -->
+<footer class="w3-container w3-padding-5 w3-center w3-opacity">  
+  <div class="w3-xlarge w3-padding-32">
+  <a href="https://www.facebook.com/profile.php?id=100004114149387" target="_blank"><i class="fa fa-facebook-official w3-hover-opacity"></i></a>
+  <a href="https://www.instagram.com/nuengwarayut/" target="_blank"><i class="fa fa-instagram w3-hover-opacity"></i></a>
+  <a href="https://www.youtube.com/channel/UCFYt2ritCwm38J1HLGNSfRg?view_as=subscriber" target="_blank"> <i class="fa fa-youtube w3-hover-opacity"></i></a>
+  <a href="https://twitter.com/Warayut25179889?s=09&fbclid=IwAR0MRCtW7dmm8Kw7kk7IuwUIVw6hheYZGDDHSxlk51yY_dRNm5rRsS8VuvQ" target="_blank"> <i class="fa fa-twitter w3-hover-opacity"></i></a>
+ </div>
+</footer>
+
+<script>
+// Used to toggle the menu on small screens when clicking on the menu button
+function myFunction() {
+  var x = document.getElementById("navDemo");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else { 
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
+</script>
+
+</body>
+</html>
---
+++ Northwind/pro_s1connect.php
@@ -0,0 +1,24 @@
+<?php
+ini_set('display_errors', 0); 
+$host = "localhost"; 
+$uname = "root";
+$upass 		= "";
+$db = "db_northwind"; 
+$tb	= "productsTast";
+$dbstatus 	= 0;
+myConnect();
+       
+function myConnect() {
+	global $db,$host,$uname,$upass,$connect,$dbstatus;	
+    //echo  $db,$host,$uname,$upass,$connect,$dbstatus;
+	if((int)phpversion() >= 7) {
+		$connect = new mysqli($host, $uname, $upass, $db);
+		if ($connect->connect_error) $connect = new mysqli($host, $uname, $upass); else $dbstatus = 1;
+        
+		if ($connect->connect_error) die("Connection failed: " . $connect->connect_error);
+	} else {
+		if(!$connect = mysql_connect($host,$uname,$upass)) die("Connect failed : ");
+          
+	}
+}
+?>
